90e0cc7acb0db0e734db1cfc4e2f6794faa6f503
ayush <sblayush@gmail.com>
2018-07-12 18:15:24 +0530

    added procfile

--- app.py
+++ app.py
@@ -1,29 +1,18 @@
-import sys
-import os
-parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/'))
-sys.path.append(parent_dir)
-from flask import Flask, render_template, request, Response
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
-import json
 
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-@app.route('/DialogFlow/Test', methods=['POST'])
-def post_dialogflow_test():
-	print("Got request for DialogFlow!")
-	resp_obj = {
-		'speech': 'This is a speech. Temperature of Blr is 0C',
-		'displayText': 'This is a speech. Temperature of Blr is 0C',
-		'source': 'Temp API'
-	}
-	resp = Response(json.dumps(resp_obj).encode('utf-8'), status=200, mimetype='application/json')
-	resp.headers['Access-Control-Allow-Origin'] = '*'
-	resp.headers['Content-Type'] = 'application/json'
-	return resp
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
 
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
-def start_server():
-	app.run()
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
 
-
-if __name__ == "__main__":
-	start_server()
