5e47ffb062aeacf4ae54205a378d4906309fb03b
ayush <sblayush@gmail.com>
2018-07-12 17:21:02 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import sys
+import os
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/'))
+sys.path.append(parent_dir)
+from flask import Flask, render_template, request, Response, session
+from flask_session import Session
+from datetime import timedelta
+app = Flask(__name__)
+import json
+from flask_cors import CORS, cross_origin
+
+cors = CORS(app, resources={r"/uploadFile": {"origins": "*"}, r"/nextPage": {"origins": "*"}})
+
+sess = Session()
+sess.init_app(app)
+@app.route('/DialogFlow/Test', methods=['POST'])
+def post_dialogflow_test():
+	print("Got request for DialogFlow!")
+	resp_obj = {
+		'speech': 'This is a speech. Temperature of Blr is 0C',
+		'displayText': 'This is a speech. Temperature of Blr is 0C',
+		'source': 'Temp API'
+	}
+	resp = Response(json.dumps(resp_obj).encode('utf-8'), status=200, mimetype='application/json')
+	resp.headers['Access-Control-Allow-Origin'] = '*'
+	resp.headers['Content-Type'] = 'application/json'
+	return resp
+
+
+def start_server(port=443):
+	app.run(host="0.0.0.0", port=port,ssl_context='adhoc')
+
+
+if __name__ == "__main__":
+	start_server()
